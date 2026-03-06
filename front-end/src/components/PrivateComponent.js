80a7c4e4319aa9c19aec78a92f803550b8a7267f
Krishna Verma <krishnaverma10904@gmail.com>
2026-03-06 11:18:52 +0530

    fix private route redirect

--- front-end/src/components/PrivateComponent.js
+++ front-end/src/components/PrivateComponent.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Navigate, Outlet} from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom';
 
-const PrivateComponent= ()=>{
+const PrivateComponent = () => {
     const auth = localStorage.getItem('user');
-    
-    return auth ?<Outlet />:<Navigate to="signup" />
+
+    return auth ? <Outlet /> : <Navigate to="/signup" />;
 }
 
-export default PrivateComponent
+export default PrivateComponent;
